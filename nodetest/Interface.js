fd8b40ab92f1c28655ffbfc5d70b78355669c56e
CHEN XIAOFENG <chenxiaofeng293@gmail.com>
2022-05-27 18:28:51 +0900

    2022.5.27 18:00

--- nodetest/Interface.js
+++ nodetest/Interface.js
@@ -9,6 +9,26 @@ const router = require('./apiRouter')
 //配置表单数据的中间件
 app.use(express.urlencoded({extended:false}))
 
+//必须在配置cors这个中间件之前，配置JSONP的接口
+app.get('/api/jsonp',(req, res) => {
+    //TODO 定义JSONP接口的具体实现过程
+    //1. 得到函数的名称
+
+    const funcName = req.query.callback
+
+    //2. 定义要发送到客户端的数据对象
+
+    const data = {name:'zs', age:22}
+
+    //3. 拼接出一个函数的调用
+
+    const scriptStr = `${funcName}(${JSON.stringify(data)})`
+
+    //4. 把拼接的字符串，响应给客户端
+
+    res.send(scriptStr)
+})
+
 //一定要在路由之前，配置cors这个中间件，从而解决跨域的问题
 const cors = require('cors')
 app.use(cors())
